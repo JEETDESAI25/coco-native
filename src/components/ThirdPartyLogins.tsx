3f6105a2a789666e6fed5355b9aef82caccd6b5a
Jeet <113221510+JEETDESAI25@users.noreply.github.com>
2024-09-06 12:18:15 -0700

    solved icon issue in ThirdPartyLogins component.

--- src/components/ThirdPartyLogins.tsx
+++ src/components/ThirdPartyLogins.tsx
@@ -27,8 +27,6 @@ interface IconButtonProps {
 }
 
 const ThirdPartyIcons: Record<number, JSX.Element> = {
-  // Alternative: (Might need resizing)
-  // [ThirdParty.Google]: <NWImage source={require('../images/google-signin-small.png')} />,
   [ThirdParty.Google]: <FontAwesomeIcon size={24} name="google" />,
   [ThirdParty.Meta]: <FontAwesomeIcon size={24} name="facebook" />,
   [ThirdParty.Microsoft]: <FontAwesomeIcon size={24} name="microsoft" />,
@@ -44,14 +42,14 @@ function IconButton(props: IconButtonProps): JSX.Element {
     <NWTouchableHighlight
       className=" bg-[#A62A72FF] rounded-lg py-2 px-3 "
       onPress={() => {
-        navigator.navigate(NavigatorTerms.DUO);
+        navigator?.navigate(NavigatorTerms.DUO);
       }}>
-      {ThirdPartyIcons[party]}
+      {ThirdPartyIcons[party as number]}
     </NWTouchableHighlight>
   );
 }
 
-export default function ThirdPartyLogins(props: any): JSX.Element {
+export default function ThirdPartyLogins(): JSX.Element {
   return (
     <NWView className=" flex-0 flex-row items-center justify-between w-[60%] py-2 pr-1 ">
       {getEnumValues(ThirdParty).map((thirdParty, index) => (
