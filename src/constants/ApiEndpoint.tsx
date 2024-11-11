ea38e8e75bf468846be802de16b8116c060a9ea6
Jeet <113221510+JEETDESAI25@users.noreply.github.com>
2024-11-10 22:45:03 -0800

    Add Google login endpoint to ApiEndpoint constants

--- src/constants/ApiEndpoint.tsx
+++ src/constants/ApiEndpoint.tsx
@@ -1,7 +1,7 @@
 export const flaskServerRoot = 'http://localhost:8000/';
-//TODO put in if else for android
 export const addUser = flaskServerRoot + 'coco/api/v1.0/add_user';
 export const register = flaskServerRoot + 'register';
 export const login = flaskServerRoot + 'login';
 export const forgot = flaskServerRoot + 'forgot';
 export const reset = flaskServerRoot + 'reset';
+export const googleLogin = flaskServerRoot + 'login/google';
